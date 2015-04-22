89d396a25b428af69c29dc4a9a8564d5473e0fee
Cyril Pereira <cyril.pereira@gmail.com>
2015-04-22 14:46:17 +0200

    Added guzzle

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -5,15 +5,24 @@ namespace ApiBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use GuzzleHttp\Client;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/guzzle")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+
+      $client = new Client();
+      $res = $client->get('http://fosrest.local/api/v1/test.json');
+      var_dump("code : ",$res->getStatusCode());
+      var_dump("type : ",$res->getHeader('content-type'));
+      var_dump("body : ",$res->getBody());
+      var_dump("json : ",$res->json());
+
+      return array('name' => "world");
     }
 }
